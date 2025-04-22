f58a8b1f41c6946ad34614d2420144f73f0b985a
yjainisc <yjain@corecard.com>
2025-04-22 19:38:42 +0530

    Update Week 3 Multi-Table Database - Tracks.py
    Updated as per the new Assignment. Also left at the bottom was the old assignment.

--- 4-Using-Database-With_Python/Assignment/Week 3 Assignment Multi-Table Database - Tracks/Week 3 Multi-Table Database - Tracks.py
+++ 4-Using-Database-With_Python/Assignment/Week 3 Assignment Multi-Table Database - Tracks/Week 3 Multi-Table Database - Tracks.py
@@ -1,11 +1,11 @@
 """
 To get credit for this assignment, perform the instructions below and upload your SQLite3 database here:
-
+No file chosen
 (Must have a .sqlite suffix)
 You do not need to export or convert the database - simply upload the .sqlite file that your program creates. See the example code for the use of the connect() statement.
 
 Musical Track Database
-This application will read an iTunes export file in XML and produce a properly normalized database with this structure:
+This application will read an iTunes export file in CSV format and produce a properly normalized database with this structure:
 
 CREATE TABLE Artist (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -24,7 +24,7 @@ CREATE TABLE Album (
 );
 
 CREATE TABLE Track (
-    id  INTEGER NOT NULL PRIMARY KEY
+    id  INTEGER NOT NULL PRIMARY KEY 
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
@@ -33,23 +33,109 @@ CREATE TABLE Track (
 );
 If you run the program multiple times in testing or with different files, make sure to empty out the data before each run.
 
-You can use this code as a starting point for your application: http://www.py4e.com/code3/tracks.zip. The ZIP file contains the Library.xml file to be used for this assignment. You can export your own tracks from iTunes and create a database, but for the database that you turn in for this assignment, only use the Library.xml data that is provided.
+You can use this code as a starting point for your application: http://www.py4e.com/code3/tracks.zip. The ZIP file contains the tracks.csv file to be used for this assignment. You can export your own tracks from iTunes and create a database, but for the database that you turn in for this assignment, only use the tracks.csv data that is provided. You can adapt the tracks_csv.py application in the zip file to complete the assignment.
 
 To grade this assignment, the program will run a query like this on your uploaded database and look for the data it expects to see:
 
-SELECT Track.title, Artist.name, Album.title, Genre.name
-    FROM Track JOIN Genre JOIN Album JOIN Artist
-    ON Track.genre_id = Genre.ID and Track.album_id = Album.id
+SELECT Track.title, Artist.name, Album.title, Genre.name 
+    FROM Track JOIN Genre JOIN Album JOIN Artist 
+    ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
         AND Album.artist_id = Artist.id
     ORDER BY Artist.name LIMIT 3
-The expected result of the modified query on your database is:
-Select Language​▼
+The expected result of the modified query on your database is: (shown here as a simple HTML table with titles)
 Track	Artist	Album	Genre
 Chase the Ace	AC/DC	Who Made Who	Rock
 D.T.	AC/DC	Who Made Who	Rock
 For Those About To Rock (We Salute You)	AC/DC	Who Made Who	Rock
-"""
 
+"""
+# -- This will read CSV file to DB   -------------------------------------------------------------
+import sqlite3
+
+conn = sqlite3.connect('trackdb3.sqlite')
+cur = conn.cursor()
+
+# Make some fresh tables using executescript()
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY 
+        AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER,
+    rating INTEGER,
+    count INTEGER
+);
+''')
+
+handle = open('tracks.csv')
+# Track        , Artist, Album      ,length ,count ,rating ,genre
+#Another One Bites The Dust,Queen,Greatest Hits,55,100,217103,Rock
+#Chase the Ace,AC/DC   ,Who Made Who,93     ,0     ,181420 ,Rock
+
+for line in handle:
+    line = line.strip();
+    pieces = line.split(',')
+    if len(pieces) < 7 : continue
+
+    track_name = pieces[0]
+    artist = pieces[1]
+    album = pieces[2]
+    length = pieces[3]
+    count = pieces[4]
+    rating = pieces[5]
+    genre = pieces[6]
+
+    print(track_name, artist, album, length, count, rating, genre)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre  (name) 
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( track_name, album_id, genre_id, length, rating, count ) )
+
+    conn.commit()
+
+
+
+#------------------------------------------------------- tHIS IS TO READ xml daTA -- oLD
 import sqlite3
 import xml.etree.ElementTree as ET
 
